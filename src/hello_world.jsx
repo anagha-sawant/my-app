c6412b2a9e32ad4d16c325a242ec5c43d7b4e066
anagha-sawant <anghasawant7@gmail.com>
2021-05-03 13:40:33 +0530

    added iteration and style to div content

---
+++ src/hello_world.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class HelloWorld extends React.Component {
+    render() {
+
+        const itemArr = [{name: "anagha", age: 25},{name: "manasi", AGE: 24},{name: "shweta", age: 26}]
+
+        const iterateBlog = itemArr.map((item, index) => {
+
+        return(
+            <div key={index}>
+               <h6>{item.name}</h6> 
+            </div>
+            )
+        })
+        return (
+            <div className="main" style={
+                {
+                    color: "red",
+                    margin: "16px",
+                    boxSizing: 'border-box',
+                    borderRadius: '5px',
+                    boxShadow: '0 2px 5px #ccc',
+                    padding: '16px'
+                }
+            }>
+                <div>
+                    {iterateBlog}
+                </div>
+                
+            </div>
+
+
+
+        )
+    }
+}
+
+export default HelloWorld
